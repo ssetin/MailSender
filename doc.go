16ba568a66a2b777ae8f9bb321b7da6f4a631012
Setin Sergei <ssetin@users.noreply.github.com>
2019-02-23 14:03:58 +0300

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,10 @@
+// test comment 3
+
+/*
+  Mailsender can be used in your reports or notification subsystem as a email queue processing tool.
+  It helps to decrease loading on smtp server in case of plenty messages. To send email you should add new record to table 
+  LETTER_TO_SEND in your database. Mailsender checks for new messages every specified period of time and send them by small portions.
+
+*/
+
+package main
